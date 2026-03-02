f41fc4a99dd94ba5bbb22f30d0e71335351fb304
dado <162951673+year41@users.noreply.github.com>
2026-03-02 15:42:11 +1100

    Edit index and create js file
    Create a boilerplate with basic information and move all the js code to
    a js file for clarity.

---
+++ script.js
@@ -0,0 +1,59 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice == 0) {
+        return "Rock";
+    } else if (choice == 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+};
+function getHumanChoice() {
+    let choice = prompt("Please write your choice between Rock, Paper or Scissors.");
+    return choice;
+};
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        const human = humanChoice.toLowerCase();
+        const computer = computerChoice.toLowerCase();
+    
+        if (human === computer) {
+            console.log("That's a draw.");
+            return;
+        } else if (human === 'rock' & computer === 'scissors') {
+            console.log('You win! Rock beats Scissors');
+            return humanScore ++;
+        } else if (human === 'paper' & computer === 'rock'){
+            console.log('You win! Paper beats Rock');
+            return humanScore ++;
+        } else if (human === 'scissors' & computer === 'paper'){
+            console.log('You win! Scissors beats Paper');
+            return humanScore ++;
+        } else {
+            console.log(`You lost! ${computer} beats ${human}`);
+            return computerScore ++;
+        }
+    };
+
+    for (let round = 1; round < 6; round++) {
+        console.log('Round: ', round);
+        // playRound('paper', 'rock');
+        playRound(getHumanChoice(), getComputerChoice());
+    };
+
+    console.log('Your score = ', humanScore);
+    console.log('Computer score = ', computerScore);
+
+    if (humanScore > computerScore) {
+        console.log("You won. Let's party!!!");
+    } else if (computerScore > humanScore) {
+        console.log("You lose, buuu!!!");
+    } else {
+        console.log("It's a draw. Try again, better luck next time!");
+    }
+};
+
+playGame();
